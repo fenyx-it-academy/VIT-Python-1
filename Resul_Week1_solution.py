22e89885e5016c39c295aa3f20f9138dc2056cf6
Sperwer10 <126902916+Sperwer10@users.noreply.github.com>
2023-03-07 14:48:24 +0100

    Update Resul_Week1_solution.py

--- Resul_Week1_solution.py
+++ Resul_Week1_solution.py
@@ -49,3 +49,47 @@ print("I", ' '*len(Yazi), "I", sep="")
 print("I", '*'*len(Yazi), "I", sep="")
 
 #Soru 6 cevabi
+while True:
+    Start = input("Hallo! Heb je zin om met mij te praten? (Ja/Nee) ")
+    if Start.lower() == "ja":
+        print("Geweldig! Ik kijk ernaar uit om met iemand te praten.")
+    elif Start.lower() == "nee":
+        print("Wat jammer! Ik voel me erg verdrietig.")
+        Tweede = input("Wil je me niet steunen? (Ja/Nee) ")
+        if Tweede.lower() == "ja":
+            print("Wat jammer! Dag.")
+            exit()
+        else:
+            print("Heel bedankt.")
+    else:
+        print("Sorry, ik begrijp niet. Probeer het opnieuw.")
+        continue
+
+    while True:
+        Derde = int(
+            input("Je mag met mij praten als je volwassen bent. Hoe oud ben je? "))
+        if Derde >= 18:
+            print("Je bent al {}. Perfecte leeftijd!" .format(Derde))
+        else:
+            print("Je bent pas ", Derde, ".",
+                  "Je mag niet met mij praten behalve onder begeleiding van je ouders.", sep="")
+            print("Je kan later met mij praten onder toezicht van je ouders.")
+            exit()
+        Vierde = input("Hoe heet je of hoe kan ik je noemen? ")
+        print("Merhaba ", Vierde, "!", sep="")
+        Vijfde = input("Wat wil je graag doen in je vrije tijd? ")
+        print("Leuk! Ik houd van wandelen in het park en lezen in mijn vrije tijd.")
+        Zesde = input("Wat is het laatste boek dat je hebt gelezen?")
+        print("Oh, ik heb dat boek ook gelezen! Wat vond je ervan?")
+        while True:
+            Zevende = input("Heb je het boek leuk gevonden? (Ja/Nee)")
+            if Zevende.lower() == "ja":
+                print("Ik ben blij dat je het leuk vond! Ik heb het ook leuk gevonden.")
+            elif Zevende.lower() == "nee":
+                print("Jammer dat je het niet leuk vond.")
+            else:
+                print("Ik begrijp niet.")
+                continue
+            print("Het was leuk om met je te praten, ",
+                  Vierde, ".", " Tot ziens!", sep="")
+            exit()
