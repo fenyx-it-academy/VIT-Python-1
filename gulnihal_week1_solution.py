4c3ffaef4d3e5fbc1c0e12d2a989d5429b40890a
gnihalozkan <126766237+gnihalozkan@users.noreply.github.com>
2023-03-14 10:35:46 +0100

    Update gulnihal_week1_solution.py

--- gulnihal_week1_solution.py
+++ gulnihal_week1_solution.py
@@ -1,1 +1,16 @@
+#soru 1
+#Dairenin alanini hesaplamak icin ilk once kullanicidan input fonk 
+#kullanarak bir yaricap alalim. 
+#Ayrica daha sonra matematik islemi ypacagimiz 
+#icin bu degeri int a cevirmeliyiz.
+yaricap= int(input("Dairenin yaricapini cm cinsinden giriniz."))
+#alan formulunu bir degere atiyoruz.
+daireninAlani=3*yaricap**2
+#bilgisayardan aldigimiz degeri alti cizili olarak yazdirmak icin 
+#bir cikti degeri olusturuyoruz. 
+cikti= "Dairenin alani"+" "+str(daireninAlani)+" "+"cm2dir."
+#bu degerin eleman sayisi kadar altini cizmek icin de len fonk kullaniyoruz.
+print(cikti,"\n","-"*len(cikti))
+
+#soru 2
 
