6e7555f911ac57436cc40fa474011271d1972ea2
gnihalozkan <126766237+gnihalozkan@users.noreply.github.com>
2023-03-14 11:17:05 +0100

    Update gulnihal_week1_solution.py

--- gulnihal_week1_solution.py
+++ gulnihal_week1_solution.py
@@ -5,12 +5,13 @@
 #icin bu degeri int a cevirmeliyiz.
 yaricap= int(input("Dairenin yaricapini cm cinsinden giriniz."))
 #alan formulunu bir degere atiyoruz.
-daireninAlani=3*yaricap**2
+daireninAlani= 3.1415*(yaricap*yaricap)
 #bilgisayardan aldigimiz degeri alti cizili olarak yazdirmak icin 
 #bir cikti degeri olusturuyoruz. 
 cikti= "Dairenin alani"+" "+str(daireninAlani)+" "+"cm2dir."
 #bu degerin eleman sayisi kadar altini cizmek icin de len fonk kullaniyoruz.
-print(cikti,"\n","-"*len(cikti))
+print(cikti,"\n", "-"*len(cikti), sep="")
 
 #soru 2
+
 
