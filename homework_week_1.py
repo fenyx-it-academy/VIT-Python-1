460883e56f3550ebb8dad1d009b704decdc94a0e
Cafer ERDEM <71823095+cfrdm@users.noreply.github.com>
2023-03-16 18:27:58 +0100

    Update homework_week_1.py

--- homework_week_1.py
+++ homework_week_1.py
@@ -1,1 +1,65 @@
+#question1
 
+r = int(input("Lütten dairenin yarı çapını cm cinsinden giriniz.. :"))
+
+formul = 3.14 * (r*r)
+
+formul = str(formul)
+
+print("Dairenin alanı", formul ,"cm2'dir.")
+print(" "*len("Dairenin alanı"),"-"*len(formul))
+
+#question2
+
+ad = input("Adınızı giriniz. :")
+soyad = input("Soyadınız giriniz.")
+
+print("Merhaba", soyad, ad, sep = "   ")
+
+
+
+
+#question3
+
+ad = input("Adınızı giriniz. :")
+soyad = input("Soyadınız giriniz. :")
+sehir = input("Yaşadığınız şehri giriniz :")
+print(ad, soyad, sehir, "*"*len(sehir), sep = "\n" )
+
+#question4
+
+saniye = int(input("Saniye giriniz :"))
+
+print(saniye,"saniye\n" , saniye/86400, "gündür.\n" , saniye / 3600, "saattir.\n", saniye/60,"dakikadır.")
+
+
+#question5
+
+ad = input("Adınızı giriniz. :")
+soyad = input("Soyadınız giriniz. :")
+
+print("|", "*"*(len(ad)+len(soyad)+1), "|", sep = "")
+print("|", " "*(len(ad)+len(soyad)+1), "|", sep = "")
+print("|", ad," ", soyad, "|" , sep = "")
+print("|", " "*(len(ad)+len(soyad)+1), "|", sep = "")
+print("|", "*"*(len(ad)+len(soyad)+1), "|", sep = "")
+
+
+
+#question6
+
+ad = input("Sohbet etmek için adınızı giriniz. :")
+print("Sayın", ad ,"hoşgeldiniz.")
+
+answer1 = input("Nasılsınız ?")
+print("bende iyiyim.")
+
+answer2 = int(input("kaç yaşındasınız ?"))
+myage = 15
+print("Aramızdaki yaş farkı", answer2-myage)
+
+county = input ("Nerelisiniz ?")
+print("güzel memleket")
+
+soyad = input("Soyadınız giriniz. :")
+print("Soyadınız : ", soyad)
